af2abfdd5f3f1a48a0721f79bf184e25c74fefbc
Orlando Caraballo <orlandocaraballo@gmail.com>
2019-04-19 15:03:55 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# backups
+
+Repository to house rsync back scripts
+
+## Install
+
+Run the following commands:
+
+```bash
+# set our file to be executable
+chmod +x install.sh
+
+# execute the installer script
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+read -p 'Remote username: ' REMOTE_USERNAME
+read -p 'Remote ip: ' REMOTE_IP
+
+# TODO: fix the following code to only accept the following two values
+read -p 'How often would you like to backup: (hourly, daily)' CADENCE
+
+# local username, local hostname, remote username, remote ip
+echo $USER, $HOSTNAME, $REMOTE_USERNAME, $REMOTE_IP, $CADENCE
+
+# create a new file based on the example version
+cp backup.example backup
+
+# run several search and replacements for our variables
+sed -i -e "s/\[local-username\]/$USER/g" ./backup
+sed -i -e "s/\[local-hostname\]/$HOSTNAME/g" ./backup
+sed -i -e "s/\[remote-username\]/$REMOTE_USERNAME/g" ./backup
+sed -i -e "s/\[remote-ip\]/$REMOTE_IP/g" ./backup
+
+# set backup script to be executable
+chmod +x backup
+
+# TODO: check if /etc/cron.hourly or /etc/cron.daily exist if not create
+# TODO: move backup script to /etc/cron.hourly or /etc/cron.daily
